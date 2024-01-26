aab2b0fba1c10248855009a909448ccc699d2796
mkheck13 <mkheck13@gmail.com>
2024-01-26 12:25:31 -0800

    First push

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+let helloInput = document.getElementById("helloInput");
+let helloOutput = document.getElementById("helloOutput");
+
+let a2Input1 = document.getElementById("a2Input1");
+let a2Input2 = document.getElementById("a2Input2");
+let a2Output = document.getElementById("a2Output");
+
+let twoqInput1 = document.getElementById("twoqInput1");
+let twoqInput2 = document.getElementById("twoqInput2");
+let twoqOutput = document.getElementById("twoqOutput");
+
+let greatInput1 = document.getElementById("greatInput1");
+let greatInput2 = document.getElementById("greatInput2");
+let greatOutput = document.getElementById("greatOutput");
+
+let madName = document.getElementById("madName");
+let madCity = document.getElementById("madCity");
+let madDrink = document.getElementById("madDrink");
+let madWeather = document.getElementById("madWeather");
+let madOutput = document.getElementById("madOutput");
+
+let oddInput = document.getElementById("oddInput");
+let oddOutput = document.getElementById("oddOutput");
+
+let alphaInput = document.getElementById("alphaInput");
+let alphaOutput = document.getElementById("alphaOutput");
+
+let numberInput = document.getElementById("numberInput");
+let numberOutput = document.getElementById("numberOutput");
+
+let magic8Input = document.getElementById("magic8Input");
+let magic8Output = document.getElementById("magic8Output");
+
+let foodInput = document.getElementById("foodInput");
+let foodOutput = document.getElementById("foodOutput");
+
