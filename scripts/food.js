52f571be2bbc356a57021839e8789939fa3a99f2
mkheck13 <mkheck13@gmail.com>
2024-01-27 01:05:28 -0800

    food picker coded, reverse num and food picker not displaying

---
+++ scripts/food.js
@@ -0,0 +1,31 @@
+let foodInput = document.getElementById("foodInput");
+let foodOutput = document.getElementById("foodOutput");
+let submitBtn = document.getElementById("submitBtn");
+
+
+
+async function PickerEndpoint(foodInput){
+    const promise = await fetch(`https://all4oneapi.azurewebsites.net/Food/${foodInput}`);
+    const data = await promise.text();
+    return data;
+}
+
+submitBtn.addEventListener('click', async () => {
+    if(foodInput.value){
+        let getData = await PickerEndpoint(foodInput.value);
+        foodOutput.textContent = getData;
+    }
+});
+
+foodInput.addEventListener('keydown', async (event) => {
+    if(foodInput.value){
+        if(event.key === "Enter"){
+            let getData =  await PickerEndpoint(event.target.value);
+            foodOutput.textContent = getData;
+        }
+    }
+});
+
+
+
+//  (`https://all4oneapi.azurewebsites.net/Food/${choice}`);
